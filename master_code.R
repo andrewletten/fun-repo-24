0794009501886aa097c3401059c0f892292f010c
Andrew Letten <andrewletten@gmail.com>
2024-02-15 13:24:51 +1000

    complete rewrite of code

--- master_code.R
+++ master_code.R
@@ -1,12 +1,3 @@
 ## This is going to be the main script file for this project
 
-library(tidyverse)
-library(boilrdata)
-
-marsupialsTibble <- as_tibble(marsupials) %>% filter(Genus == "Acrobates")
-
-summary(lm(MaxLifeSpan ~ Mass, data = marsupialsTibble))
-ggplot(marsupialsTibble, aes(y = MaxLifeSpan, x = Mass)) +
-  geom_point()
-
-# more cod here...
+# I don't like this script 
